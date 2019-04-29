a1766fd5b608b88a91ba0deb723ad4a882a7f7bb
doyen <jardinerodney@gmail.com>
2019-04-29 18:40:02 +0100

    creating router files using express

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ connectDB();
 // port listen
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
+// SERVER ROUTES - Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 // TEST GET REQ
 app.get("/", (req, res) => res.send("API RUNNING"));
