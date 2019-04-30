ad536b0550dfdbcd4d19b2009567b22207baa4ea
doyen <jardinerodney@gmail.com>
2019-04-30 22:43:49 +0100

    create and be able to update profile route made

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const db = config.get("mongoURI");
 const connectDB = async () => {
   // run mongoseconnect (async works with try catch )
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     console.log("mongoDB connected...");
   } catch (err) {
     console.log(err.message);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
 // model
 const ProfileModel = require("../../models/Profile");
 const UserModel = require("../../models/User");
+
 // @route   GET api/profile/me
 // @desc    get current users profile private
 // @access  private
@@ -25,5 +28,92 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// @route   POST api/profile
+// @desc    create or update user profile
+// @access  private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills are required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //   error checking
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // pull field out of body
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+    // check fields
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    // main profile setup and edit
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    // skills to array
+    if (skills) {
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+    //social links buld social object
+    profileFields.social = {};
+    // checks
+    if (youtube) profileFields.social.youtube = youtube;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (twitter) profileFields.social.twitter = twitter;
+
+    // update and insert the data
+    try {
+      let profile = await ProfileModel.findOne({ user: req.user.id });
+      // look for profile and update
+      if (profile) {
+        profile = await ProfileModel.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        // return profile
+        return res.json(profile);
+      }
+
+      //create profile if not found
+      profile = new ProfileModel(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
