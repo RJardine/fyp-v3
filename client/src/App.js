16293b03d9b351e810e5157d8d4050525eaf82e1
doyen <jardinerodney@gmail.com>
2019-05-01 14:36:56 +0100

    created a redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 // pages
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/home/Home";
@@ -8,16 +11,18 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Switch>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
