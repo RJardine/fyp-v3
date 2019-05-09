e13c349b8e4d4cf0ea9bdee7a76fcc2d2e0105b1
doyen <jardinerodney@gmail.com>
2019-05-09 09:12:34 +0100

    Deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const path = require("path");
 
@@ -11,9 +10,6 @@ app.use(express.json({ extended: false }));
 
 // db connect
 connectDB();
-
-// port listen
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // SERVER ROUTES - Define Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -30,3 +26,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+const PORT = process.env.PORT || 5000;
+
+// port listen
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
