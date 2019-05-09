83001fa6070d9b5495425de8262f27a5f71786f1
doyen <jardinerodney@gmail.com>
2019-05-09 09:39:37 +0100

    deployment test 3

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use("/api/profile", require("./routes/api/profile"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // set static folder
+  // Set static folder
   app.use(express.static("client/build"));
-  // route
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
